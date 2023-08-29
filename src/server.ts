cecac6e1bab4411454485c6738cd1390d32ee1e4
Philipe Faria <philipefaria61@gmail.com>
2023-08-29 15:49:50 -0300

    create transactions

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,16 @@
-import crypto from 'node:crypto'
-
 import fastify from 'fastify'
-import { knex } from './database'
+import { env } from './env'
+
+import { transactionsRoutes } from './routes/transactions'
 
 const server = fastify()
 
-server.get('/', async () => {
-  const transactions = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 1000,
-  })
-
-  return transactions
+server.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 server
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Server is running'))
